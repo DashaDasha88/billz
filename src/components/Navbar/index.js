2bd93a3cc9672c08a20e375396c23bfb7c613340
Dasha <dasha.kotova.mail@gmail.com>
2021-08-30 17:21:05 -0400

    add navbar styles

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import { Nav, NavbarContainer, NavLogo } from './NavbarElements';
+import { FaBars } from "react-icons/fa";
+import { 
+  Nav, 
+  NavbarContainer, 
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+} from './NavbarElements'; 
 
 const Navbar = () => {
   return (
@@ -10,6 +19,13 @@ const Navbar = () => {
         <MobileIcon>
           <FaBars />
         </MobileIcon>
+
+        <NavMenu>
+          <NavItem>
+            <NavLinks to="about">About</NavLinks>
+          </NavItem>
+        </NavMenu>
+
       </NavbarContainer>
     </Nav>
     </>
